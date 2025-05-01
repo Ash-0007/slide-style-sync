47db545a64d0446563c241ff8ea0092482c2a8ca
Ash-004 <ash_004@icloud.com>
2025-05-01 13:12:44 +0530

    more changes

--- src/services/PresentationService.ts
+++ src/services/PresentationService.ts
@@ -2,7 +2,7 @@ import { toast } from "sonner";
 
 
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://dahmercal.pythonanywhere.com';
 
 
 

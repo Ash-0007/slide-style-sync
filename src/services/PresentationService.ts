aad9a7b4884dcb8a784d721429b9223366ea24e4
Ash-0007 <165654136+Ash-0007@users.noreply.github.com>
2025-05-01 14:18:36 +0530

    api

--- src/services/PresentationService.ts
+++ src/services/PresentationService.ts
@@ -2,7 +2,7 @@ import { toast } from "sonner";
 
 
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://dahmercal.pythonanywhere.com';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://slide-style-sync.onrender.com';
 
 
 
